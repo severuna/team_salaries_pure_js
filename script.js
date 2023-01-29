10e51031b7d37150edaa6450a20ea69e55d6b53e
Severuna <oxxxyfomenkova@gmail.com>
2023-01-29 22:21:53 +0200

    create another pure JS solution

---
+++ script.js
@@ -0,0 +1,16 @@
+/* 
+another solution is pure JS
+*/
+
+// create an example to verify the correctness of the function
+const salaries1 = {
+    Manager: { salary: 1000, tax: "10%" },
+    Designer: { salary: 600, tax: "30%" },
+    Artist: { salary: 1500, tax: "15%" },
+}
+const team1 = [
+    { name: "Misha", specialization: "Manager" },
+    { name: "Max", specialization: "Designer" },
+    { name: "Vova", specialization: "Designer"},
+    { name: "Leo", specialization: "Artist"},
+]
