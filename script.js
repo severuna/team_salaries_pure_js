2016938c4c7a6d1a90bffa8be1336681333d712b
Severuna <oxxxyfomenkova@gmail.com>
2023-01-29 22:46:46 +0200

    final edits

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ const team1 = [
     { name: "Vova", specialization: "Designer"},
     { name: "Leo", specialization: "Artist"},
 ]
+
+// create a main function where we transfer the salaries of specializations and team members
+const calculateTeamFinanceReport = (salaries, team) => {
+    let countSpecialization = {};
+    let outBudget = {};
+    let totalBudget = 0;
+    team.forEach(function(a){
+        countSpecialization[a.specialization] = countSpecialization[a.specialization] + 1 || 1;
+    });
+    for (let key in countSpecialization) {
+        outBudget[key] = countSpecialization[key] * (salaries[key].salary + salaries[key].salary * Number(String(salaries[key].tax).slice(0, 2)) / 100);
+        totalBudget += countSpecialization[key] * (salaries[key].salary + salaries[key].salary * Number(String(salaries[key].tax).slice(0, 2)) / 100);
+    }
+    outBudget['Total Budget'] = totalBudget;
+    return outBudget;
+}
+
+const financeReport1 = calculateTeamFinanceReport(salaries1, team1)
+console.log(JSON.stringify(financeReport1))
