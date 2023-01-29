f71412599cc9e9023ac82fe050b7b699e399c609
Severuna <oxxxyfomenkova@gmail.com>
2023-01-29 19:32:09 +0200

    create an example to verify the correctness of the function

--- script/index.js
+++ script/index.js
@@ -1,1 +1,12 @@
-console.log(5)
+// create an example to verify the correctness of the function
+const salaries1 = {
+    Manager: { salary: 1000, tax: "10%" },
+    Designer: { salary: 600, tax: "30%" },
+    Artist: { salary: 1500, tax: "15%" },
+}
+const team1 = [
+    { name: "Misha", specialization: "Manager" },
+    { name: "Max", specialization: "Designer" },
+    { name: "Vova", specialization: "Designer"},
+    { name: "Leo", specialization: "Artist"},
+]
